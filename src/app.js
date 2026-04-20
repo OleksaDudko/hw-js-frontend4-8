381560272da296fa071cb2882915e10854485a49
OleksaDudko <132586603+OleksaDudko@users.noreply.github.com>
2026-04-20 19:14:01 +0300

    second

--- src/app.js
+++ src/app.js
@@ -1,34 +1,51 @@
-const inputEl = document.querySelector("#bookmarkInput");
-const btnEl = document.querySelector("#addBookmarkBtn");
-const listEl = document.querySelector("#bookmarkList");
-const arr = [];
+// const inputEl = document.querySelector("#bookmarkInput");
+// const btnEl = document.querySelector("#addBookmarkBtn");
+// const listEl = document.querySelector("#bookmarkList");
+// const arr = [];
 
-btnEl.addEventListener("click", () => {
-    const data = {
-        value: inputEl.value
-    }
-    arr.push(data);
-    console.log(arr);
-    addObj(arr)
+// btnEl.addEventListener("click", () => {
+//     const data = {
+//         value: inputEl.value
+//     }
+//     arr.push(data);
+//     console.log(arr);
+//     addObj(arr)
+// })
+
+// function addObj(arr) {
+//     const item = arr.map((item) => {
+//         return `<li>
+//                     <a href="">${item.value}</a>
+//                     <button type="button">Видалити</button>
+//                 </li>`
+//     }).join("")
+//     listEl.innerHTML = item;
+// }
+
+
+// listEl.addEventListener("click", (event) => {
+//     const btn = event.target.closest("button");
+//     if (btn) {
+//         const id = btn.dataset.id;
+//         arr.splice(id, 1);
+//         addObj(arr);
+//     }
+// });
+
+// ==========================================================
+
+const nameEl = document.querySelector("#username");
+const passwordEl = document.querySelector("#password");
+const saveEl = document.querySelector("#saveBtn");
+
+saveEl.addEventListener("click", (event) => {
+    const localName = localStorage.setItem("login", nameEl.value);
+    const localPassword = localStorage.setItem("password", passwordEl.value);
+    console.log(localName, localPassword);
 })
 
-function addObj(arr) {
-    const item = arr.map((item) => {
-        return `<li>
-                    <a href="">${item.value}</a>
-                    <button type="button">Видалити</button>
-                </li>`
-    }).join("")
-    listEl.innerHTML = item;
-}
+const savedLogin = localStorage.getItem("login");
+const savedPassword = localStorage.getItem("password");
 
-
-listEl.addEventListener("click", (event) => {
-    const btn = event.target.closest("button");
-    if (btn) {
-        const index = btn.dataset;
-        arr.splice(index, 1);
-        addObj(arr);
-    }
-});
-
+nameEl.value = savedLogin;
+passwordEl.value = savedPassword
